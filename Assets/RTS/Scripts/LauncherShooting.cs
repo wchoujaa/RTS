0f1d5c93b73ab7486f543bf841a4bd16abf72dad
wchoujaa <wchoujaa@ulb.ac.be>
2021-02-16 19:49:14 +0100

    update

---
+++ Assets/RTS/Scripts/LauncherShooting.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Turrets;
+using UnityEngine;
+
+public class LauncherShooting : MonoBehaviour {
+	private PlayerUnitController unitController;
+	public UnitStats unitStats;
+	public AAPod aapod;
+	public bool isLaunching = false;
+	public TurretRotation turretRotation;
+
+	private void Start()
+	{
+		unitController = GetComponent<PlayerUnitController>();
+		turretRotation = GetComponentInChildren<TurretRotation>();
+	}
+
+	private void Update()
+	{
+		if (unitController.targetAcquired)
+		{
+
+			turretRotation.SetIdle(false);
+
+			var currentTarget = unitController.Target;
+			var distance = (transform.position - currentTarget.position).magnitude;
+
+			if (distance <= unitStats.attackRange)
+			{
+				turretRotation.SetAimpoint(currentTarget.position);
+
+				if (turretRotation.IsLineOfSight() && !isLaunching)
+				{
+					StartCoroutine(launchMissile());
+				}
+
+			}
+		}
+		else
+		{
+			turretRotation.SetIdle(true);
+		}
+		 
+ 
+	}
+
+	IEnumerator launchMissile()
+	{
+		isLaunching = true; 
+		Transform currentTarget = unitController.Target;
+
+ 
+		aapod.Launch(currentTarget);
+		yield return new WaitForSeconds(2.5f);
+		isLaunching = false;
+	}
+}
---
+++ Assets/RTS/Turrets/GunTurrets/Scripts/TurretShooting.cs
@@ -0,0 +1,71 @@
+using GNB;
+using System.Collections;
+using System.Collections.Generic;
+using Turrets;
+ 
+using UnityEngine;
+public class TurretShooting : MonoBehaviour
+{
+
+ 
+	public UnitStats unitStats;
+	public TurretRotation turretRotation;
+	public List<Gun> guns;
+	public float velocity;
+	public bool isFiring = false;
+	[Range(1, 1000)]
+	public float Rate;
+	private PlayerUnitController unitController;
+	private void Start()
+	{
+		 
+		unitController = GetComponentInParent<PlayerUnitController>();
+	}
+
+	private void Update()
+	{
+		if (unitController.targetAcquired)
+		{
+
+			turretRotation.SetIdle(false);
+
+			var currentTarget = unitController.Target;
+			var distance = (transform.position - currentTarget.position).magnitude;
+
+			if (distance <= unitStats.attackRange)
+			{
+				turretRotation.SetAimpoint(currentTarget.position);
+
+				if (turretRotation.IsLineOfSight() && !isFiring)
+				{
+					StartCoroutine(FireBullets());
+				}
+
+			}
+		}
+		else
+		{
+			turretRotation.SetIdle(true);
+		}
+	}
+
+
+	IEnumerator FireBullets()
+	{
+		isFiring = true;
+
+		foreach(Gun gun in guns)
+		{
+			if (gun.ReadyToFire)
+			{
+				gun.Fire(unitController.transform.forward * velocity);
+				break;
+			}
+		}
+
+		yield return new WaitForSeconds(1/Rate);
+
+		isFiring = false;
+		yield return null;
+	}
+}
